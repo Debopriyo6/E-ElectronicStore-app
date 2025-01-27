b4a1edf7910bd51e8cf4d2fc787320679c5e9cc1
Debopriyo6 <debopriyoray662001@gmail.com>
2025-01-27 12:50:37 +0530

    pushing backend

--- Frontend/src/Component/Context.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-
-const itemcontext=createContext();
-const dummycontext=createContext();
-const logincontext=createContext();
-const namecontext=createContext();
-const cartcontext=createContext();
-export {itemcontext,dummycontext,logincontext,namecontext,cartcontext};
