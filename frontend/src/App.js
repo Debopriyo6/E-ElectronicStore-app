74c7c0848ee8b59c405cff2ed3a34979799e75c9
Debopriyo6 <debopriyoray662001@gmail.com>
2025-04-15 15:47:50 +0530

    updating frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter as Switch, Route, Redirect} from "react-router-dom";
+import { Switch, Route, Redirect} from "react-router-dom";
 import Home from "./Component/Home";
 import Navbar from "./Component/Navbar";
 import { itemcontext } from "./Component/Context";
