f541b6fb84bc1795aa1300be10a77a4d27f5ca4f
Debopriyo6 <debopriyoray662001@gmail.com>
2025-04-21 23:12:02 +0530

    updating frontend & backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,7 +25,7 @@ function App() {
 
 
   useEffect(() => {
-    axios.get("http://13.201.168.180:8086/backend-0.0.1-SNAPSHOT/getitem").then((response) => {
+    axios.get("http://3.109.153.234:8086/backend-0.0.1-SNAPSHOT/getitem").then((response) => {
       console.log(response);
       Setitem(response.data);
     });
@@ -35,7 +35,7 @@ function App() {
 
 
   useEffect(() => {
-    axios.get("http://13.201.168.180:8086/backend-0.0.1-SNAPSHOT/user").then((response) => {
+    axios.get("http://3.109.153.234:8086/backend-0.0.1-SNAPSHOT/user").then((response) => {
       console.log(response);
       
       setPerson(response.data);
